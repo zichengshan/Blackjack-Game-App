a2349f0f9f7d3ee856b4808379cb825bbafdb399
Zicheng.S <zichengshan@gmail.com>
2022-03-24 17:12:15 -0700

    display the message
    display the message when click on the button

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
+let messageEl = document.getElementById("message-el")
+
 function startGame() {
     if (sum <= 20) {
         message = "Do you want to draw a new card? 🙂"
@@ -13,5 +15,5 @@ function startGame() {
         message = "You're out of the game! 😭"
         isAlive = false
     }
-    console.log(message)
+   messageEl.textContent = message
 }
