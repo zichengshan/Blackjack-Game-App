550a5a53fbcbbf504c0edd4ef53ee344446f73cb
Zicheng.S <zichengshan@gmail.com>
2022-03-24 20:46:01 -0700

    Add object to save states

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-
+let player = {
+    name: "Zicheng",
+    chips: 150
+}
 let cards = []
 let sum = 0
 let hasBlackJack = false
@@ -7,6 +10,9 @@ let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.querySelector("#sum-el")
 let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = player.name + ": $" + player.chips
 
 function startGame() {
     isAlive = true
